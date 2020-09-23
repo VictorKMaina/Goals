1fe1ab80a7de8cc470acd56deca5c90865f2b1c4
Victor K. Maina <vk13runic@gmail.com>
2020-09-23 18:17:10 +0300

    finished goals app code-along

---
+++ src/app/goal/goal.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Goal } from "../goal"
+
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
+})
+export class GoalComponent implements OnInit {
+  goals:Goal[] = [
+    new Goal(1, "Watch Finding Nemo", "Find an online version", new Date(2020, 8, 30)),
+    new Goal(2, "Buy cookies", "I have to buy cookies for the parrot", new Date(2020, 8, 30)),
+    new Goal(3, "Get a new phone case", "Diana's birthday is coming up soon", new Date(2020, 8, 30)),
+    new Goal(4, "Get dog food", "Pupper likes expensive snacks", new Date(2020, 8, 30)),
+    new Goal(5, "Solve math homework", "Damn math!", new Date(2020, 8, 30)),
+    new Goal(6, "Plot my world domination!", "Because I'm an evil overlord!!!!", new Date(2020, 9, 30))
+  ];
+  toggleDetails(index){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+  completeGoal(trueOrFalse, item){
+    if (trueOrFalse == true){
+      this.goals.splice(item, 1);
+    }
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
